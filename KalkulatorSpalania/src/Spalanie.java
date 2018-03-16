35c55db814dcbb75996267199f755de7ef2ddf34
MaciejHeintze <maciej.heintze96@gmail.com>
2018-03-16 14:27:11 +0100

    Kalkulator spalania benzyny
    Aplikacja obliczająca koszt podróży + GUI

---
+++ KalkulatorSpalania/src/Spalanie.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+        import javax.swing.JFrame;
+
+
+public class Spalanie extends JFrame{
+
+    JTextField srednieSpalanie, odleglosc, liczbaPasazerow;
+    JLabel srednieSpalanieL, odlegloscL, liczbaPasazerowL;
+
+    public Spalanie()
+    {
+
+    }
+
+    public static void main (String[] args){
+
+    }
+
+}
